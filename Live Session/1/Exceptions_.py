b4ce764db79a10941f5b02447ad9c37e63662a08
Ayomide Oke <61634184+Ay-source@users.noreply.github.com>
2024-01-26 21:56:24 +0100

    Update Exceptions_.py
    Fixed exceptions.py

--- Live Session/1/Exceptions_.py
+++ Live Session/1/Exceptions_.py
@@ -1,6 +1,9 @@
 # We're expecting an integer instead of a float. This shows an exception
-integer_result = 7 / 0 #Exception occurs when the code is running
+integer_result = 7 / 7 #Exception occurs when the code is running
 print(integer_result)
+
+# Zero Division Error
+division_error = 7 / 0
 
 # Explanation of explicit is better than implicit
 """
